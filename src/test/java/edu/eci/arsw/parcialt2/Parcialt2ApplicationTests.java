f77ebf9ab1532c315db2f4e44effdb3443b65838
KyrieLW <james.weisnner@mail.escuelaing.edu.co>
2021-04-07 07:09:33 -0500

    Creando proyecto

---
+++ src/test/java/edu/eci/arsw/parcialt2/Parcialt2ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.parcialt2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parcialt2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
