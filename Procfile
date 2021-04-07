ba1d8ad5d1a2743f0a11a573389006e88811805c
KyrieLW <james.weisnner@mail.escuelaing.edu.co>
2021-04-07 10:39:50 -0500

    procfile update

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java $JAVA_OPTS -cp target/classes:target/dependency/* edu.eci.arsw.parcialt2
+web: java $JAVA_OPTS -cp target/classes:target/dependency/* edu.eci.arsw.parcialt2.Parcialt2Application
--- src/main/java/edu/eci/arsw/parcialt2/Cache/CityCache.java
+++ src/main/java/edu/eci/arsw/parcialt2/Cache/CityCache.java
@@ -1,15 +1,30 @@
 package edu.eci.arsw.parcialt2.Cache;
 
+import edu.eci.arsw.parcialt2.Model.City;
+import org.javatuples.Pair;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
 import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Cache de consultas de informacion climatica en una ciudad, contiene un hashmap donde almacena los objetos de ciudad
  */
+@Service
 public class CityCache {
-    private HashMap cachePersistence;
+    private ConcurrentHashMap<String, Pair<City, Date>> cachePersistence=new ConcurrentHashMap<>();
 
     public CityCache(){
 
+    }
+
+    public void addCache(String cityName, City cityInfo){
+        cachePersistence.put(cityName, new Pair<City, Date>(cityInfo, new Date()));
+    }
+
+    public Pair<City, Date> getCache(String name){
+        return cachePersistence.get(name);
     }
 
 
